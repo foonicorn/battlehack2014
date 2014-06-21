2116ed5bde0011e24aa2e39d9ac8b52f8efd6474
Norman Rusch <webmaster@grind-it.de>
2014-06-21 16:45:53 +0200

    Add content rendering for view

--- static/js/src/application/content/views/Content.js
+++ static/js/src/application/content/views/Content.js
@@ -1,19 +1,22 @@
 define(function(require) {
 
 	var
-		// $ = require('jquery'),
 		_ = require('underscore'),
 		Backbone = require('backbone'),
-		View = require('core/View')
+		View = require('core/View'),
+		Template = require('text!application/content/views/Content.html')
 	;
 
 	return View.extend({
+
+		_template: _.template(Template),
 
 		_init: function(options) {
 			this._views = [];
 		},
 
 		render: function() {
+			this._content = $(this._template()).appendTo(this.$el);
 			return this;
 		},
 
