69f20dcd229976388d6d04a906a62a741b78b997
Andreas Hug <hello@foonicorn.com>
2014-06-21 20:06:30 +0200

    Add model for payments

--- src/battlehack/core/models.py
+++ src/battlehack/core/models.py
@@ -28,3 +28,39 @@ class Challenge(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+TYPE_OWNER = 'owner'
+TYPE_RIVAL = 'rival'
+TYPE_CHOICES = (
+    (TYPE_OWNER, _('owner')),
+    (TYPE_RIVAL, _('rival')),
+)
+
+STATUS_CREATED = 'created'
+STATUS_EXECUTED = 'executed'
+STATUS_CAPTURED = 'captured'
+STATUS_VOIDED = 'voided'
+STATUS_FAILED = 'failed'
+STATUS_CHOICES = (
+    (STATUS_CREATED, _('created')),
+    (STATUS_EXECUTED, _('executed')),
+    (STATUS_CAPTURED, _('captured')),
+    (STATUS_VOIDED, _('voided')),
+    (STATUS_FAILED, _('failed')),
+)
+
+class Payment(models.Model):
+    challenge = models.ForeignKey(Challenge, verbose_name=_('challenge'))
+    user = models.ForeignKey('auth.User', verbose_name=_('user'))
+    type = models.CharField(_('type'), max_length=20, choices=TYPE_CHOICES)
+    pid = models.CharField(_('payment id'), max_length=100)
+    status = models.CharField(
+        _('status'), max_length=20, choices=STATUS_CHOICES)
+
+    class Meta:
+        verbose_name = _('payment')
+        verbose_name_plural = _('payments')
+
+    def __unicode__(self):
+        return self.pid
--- src/testing/tests/core/test_models.py
+++ src/testing/tests/core/test_models.py
@@ -1,7 +1,12 @@
-from testing.factories.factory_core import ChallengeFactory
+from testing.factories.factory_core import ChallengeFactory, PaymentFactory
 
 
 def test_challenge(db):
     challenge = ChallengeFactory.create()
     assert challenge.owner
     assert challenge.charity
+
+def test_payment(db):
+    payment = PaymentFactory.create()
+    assert payment.user
+    assert payment.challenge
