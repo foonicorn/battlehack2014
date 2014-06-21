d8050ae2cfd3d52dc9da08a08ee4783f80f7c3e2
Norman Rusch <webmaster@grind-it.de>
2014-06-21 14:36:49 +0200

    Add grunt connect task to test local

---
+++ resources/grunt/tasks/contrib-connect.js
@@ -0,0 +1,17 @@
+var
+	configuration = require('../configuration')
+;
+
+module.exports = function(grunt) {
+
+	grunt.config('connect', {
+		all: {
+			options: {
+				port: 9000,
+				keepalive: true
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+};
